4ac929597e1203f83f90f2c33dc99822e0b6a48e
AnjonB <anjonbhattacharjee.inbox@gmail.com>
2024-04-23 17:39:26 +0530

    Update llama2-pdf-streamlit.py
    Updated heading

--- llama2-pdf-streamlit.py
+++ llama2-pdf-streamlit.py
@@ -50,8 +50,8 @@ def main():
 ]
     
     index_placeholder = None
-    st.set_page_config(page_title = "Chat with your PDF using Llama2 & Llama Index", page_icon="🦙")
-    st.header('🦙 Chat with your PDF using Llama2 model & Llama Index')
+    st.set_page_config(page_title = "RFP Summariser", page_icon="🤖")
+    st.header('💬 With your RFP 📃')
     
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
